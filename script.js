ae4449a7951b288642d94b4c0549514bc71d7538
Nik <skitchen1992@gmail.com>
2021-04-15 12:58:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+// Функция принимает строку чисел и выводит индекс элемента, который не попадает под зависимость. Например "2 4 7 8 10" выводит третий элемент 7. 
+function iqTest(numbers){   
+    let sum = numbers.split(' ');//Преобразуем строку в массив
+    let even = 0; //Четные
+    let notEven = 0;//Не Четные
+    let point = 0;//Индекc
+
+    for (let i=0; i<sum.length; i++){//Перебираем массив
+        if(sum[i]%2== 0){
+            even  += 1; //Считаем кол-во четных цифр
+        }else{
+            notEven += 1; //Считаем кол-во нечетных цифр
+        }
+    }
+    if(notEven > even){//Если не четных больше, то нам необходимо найти индекс четного и наоборот
+        for (let i=0; i<sum.length; i++){
+            if(sum[i]%2== 0){
+                point = i+1; 
+            }
+        }    
+    }else{
+        for (let i=0; i<sum.length; i++){
+            if(sum[i]%2!== 0){
+                point = i+1; 
+            }
+        } 
+    }    
+    return point;
+    
+}
+console.log(iqTest("1 3 4 5 7"));
